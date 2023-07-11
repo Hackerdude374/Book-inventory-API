6bd93ca2ad1d982067ac6b949b1151fe44cfb497
Hackerdude374 <robertle982@gmail.com>
2023-07-11 15:23:01 -0400

    woo

---
+++ book-inventory-api/books.js
@@ -0,0 +1,12 @@
+const books = [{
+id: 1,
+author: 'Thomas foye',
+title: 'the car',
+NumOfPages: 600,
+genre: 'fiction',
+status: 1,
+
+
+
+
+}]
---
+++ book-inventory-api/database.js
@@ -0,0 +1,41 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+appendFile.use(express.json());
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+
+// ...
+
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS bookinventories (
+    id SERIAL PRIMARY KEY,
+    author VARCHAR(100) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    NumOfPages INTEGER,
+    genre VARCHAR(100) NOT NULL,
+    status INTEGER NOT NULL
+  );
+`;
+
+const createTable = async () => {
+  try {
+    await pool.query(createTableQuery);
+    console.log('Table created successfully');
+  } catch (err) {
+    console.error('Error executing query', err.stack);
+  }
+};
+
+createTable();
+module.exports = {
+    query: (text, params, callback) => {
+      console.log("QUERY:", text, params || "");
+      return pool.query(text, params, callback);
+    },
+  };
+module.exports = pool;
